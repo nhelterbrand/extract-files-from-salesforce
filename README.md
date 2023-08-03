fca17df9b8d34593d570abb88a22922868782a4c
Nate Helterbrand <nhelterbrand@gmail.com>
2023-08-03 13:33:17 -0500

    updating to put files into folders based on account name

--- README.md
+++ README.md
@@ -1,13 +1,13 @@
 # Extract Files from Salesforce
 
-A nodejs app that lets you configure a query of ContentVersion records to extract from Salesforce. The tool saves all the files into a `files` folder in the app's folder structure
+A nodejs app that lets you configure a query of ContentVersion records to extract from Salesforce and put the files into folders based on the LinkedEntity's Name. The tool saves all the files into a `files` folder in the app's folder structure
 
 ## Prep
 
 1. Make sure that node is downloaded onto your machine, this app was written with node v19.3.0
 3. run `npm install` to install the node modules defined in the `package.json`
 2. Use the `.env.example` to create a `.env` file which has the specific details for your org populated. You will need your org's domain name, username, password, and security token
-3. Edit the query to filter the ContentVersion records queried, if left as is, this app will query all ContentVersion records and download all files in the org.
+3. Edit the query to filter the linked entity records queried (could be Accounts or any parent record of a file type of object), if left as is, this app will query all ContentVersion records related to any Account and download all files in the org.
 
 ## To Run
 
--- server.js
+++ server.js
@@ -6,78 +6,153 @@ const fs = require('fs');
 const userName = process.env.USERNAME;
 const passwordWithToken = process.env.PASSWORD + process.env.SECURITY_TOKEN;
 
-var conn = new jsforce.Connection({
-	loginUrl : process.env.BASE_URL
-});
+async function main() {
+	const conn = await logIn(userName, passwordWithToken);
 
-console.log('logging in...');
-conn.login(userName, passwordWithToken, function(err, userInfo) {
-	if (err) { return console.error(err); }
+	const linkedEntityMapByContentDocumentId = await queryContentDocumentLinks(conn);
+	console.log(linkedEntityMapByContentDocumentId)
 
-	console.log('token ', conn.accessToken);
-	console.log('instance url ', conn.instanceUrl);
-	// logged in user property
-	console.log("User ID: " + userInfo.id);
-	console.log("Org ID: " + userInfo.organizationId);
+	// make folder directories and get content versions
+	const contentVersions = await makeFoldersAndQueryContentVersionsForLinkedEntities(conn, 'files', linkedEntityMapByContentDocumentId);
+	console.log(contentVersions);
 
-	var records = [];
-	conn.query("SELECT Id, Title, FileExtension, ContentDocumentId, VersionData FROM ContentVersion", function(err, result) {
-		if (err) { return console.error(err); }
-		console.log("total : " + result.totalSize);
-		console.log("fetched : " + result.records.length);
-		records = result.records;
+	// download files into folders
+	for (i in contentVersions) {
+		var record = contentVersions[i];
+		var linkedEntityName = linkedEntityMapByContentDocumentId.get(record.ContentDocumentId);
+		console.log(linkedEntityName);
+		var contentFilePath = 'files' + '/' + linkedEntityName + '/' + record.Title + '.' + record.FileExtension;
 
-		var record;
-		for (recordIndex in records) {
-			record = records[recordIndex];
-			console.log(record);
-
-			downloadFile(record.Title + '.' + record.FileExtension, record.VersionData);
-		}
-	});
-});
-
-
-function downloadFile(contentVersionFileName, path) {
-
-    const options = {
-        hostname: process.env.DOMAIN_NAME,
-        port: 443,
-        path: path,
-        method: 'GET',
-        headers: {
-            'Content-Type': 'application/octet-stream',
-            'Authorization': 'OAuth '+conn.accessToken
-        }
-    }
-
-	const folderName = 'files';
-	const filePath = folderName + '/' + contentVersionFileName
-
-    https.get(options, (resp) => {
-        let data = '';
-		if (!fs.existsSync('files')){
-			fs.mkdirSync('files');
-		}
-
-        // A chunk of data has been received.
-        resp.on('data', (chunk) => {
-            console.log(chunk);
-            data += chunk;
-
-            fs.appendFile(filePath, chunk, function (err) {
-                if (err) throw err;
-                console.log('chunk updated');
-            });
-
-        });
-
-        // The whole response has been received. Print out the result.
-        resp.on('end', () => {
-            console.log('data downloaded');
-        });
-
-    }).on("error", (err) => {
-        console.log("Error: " + err.message);
-    });
+		await downloadFile(conn, contentFilePath, record.VersionData);
+	}
 }
+
+function logIn(userName, passwordWithToken) {
+	var conn = new jsforce.Connection({
+		loginUrl : process.env.BASE_URL
+	});
+
+	console.log('logging in...');
+	let promise = new Promise(function(resolve, reject) {
+		conn.login(userName, passwordWithToken, function(err, userInfo) {
+			if (err) { reject(console.error(err)); }
+			// Now you can get the access token and instance URL information.
+			// Save them to establish connection next time.
+			console.log(conn.accessToken);
+			console.log(conn.instanceUrl);
+			// logged in user property
+			console.log("User ID: " + userInfo.id);
+			console.log("Org ID: " + userInfo.organizationId);
+			resolve(conn);
+		});
+	});
+
+	return promise;
+}
+
+function queryContentDocumentLinks(conn) {
+	var linkedEntityMapByContentDocumentId = new Map();
+
+	let promise = new Promise(function(resolve, reject) {
+
+		// if making changes, don't change the fields retrieved, just change the where clause, or maintain those fields retrieved
+		conn.query("SELECT Id, LinkedEntityId, LinkedEntity.Name, ContentDocumentID, ContentDocument.LatestPublishedVersionID FROM ContentDocumentLink WHERE LinkedEntityId IN (SELECT Id FROM Account)",
+		function(err, result) {
+			if (err) { reject(console.error(err)); }
+
+			console.log("total content documents related to accounts : " + result.totalSize);
+			console.log("fetched : " + result.records.length);
+
+			var record;
+			for (recordIndex in result.records) {
+				record = result.records[recordIndex];
+				linkedEntityMapByContentDocumentId.set(record.ContentDocumentId, record.LinkedEntity.Name);
+			}
+
+			resolve(linkedEntityMapByContentDocumentId);
+		});
+	});
+
+	return promise;
+}
+
+function makeFoldersAndQueryContentVersionsForLinkedEntities(conn, parentFolderName, linkedEntityMapByContentDocumentId) {
+	const folderName = parentFolderName;
+	if (!fs.existsSync(folderName)) {
+		fs.mkdirSync(folderName);
+	}
+
+	for (var linkedEntity of linkedEntityMapByContentDocumentId.entries()) {
+		var value = linkedEntity[1];
+		var filePath = folderName + '/' + value;
+
+		if (!fs.existsSync(filePath)) {
+			fs.mkdirSync(filePath);
+		}
+	}
+
+	let promise = new Promise(function(resolve, reject) {
+		var records = [];
+		var idList = '';
+		var contentDocumentIdList = Array.from(linkedEntityMapByContentDocumentId.keys());
+		for (i in contentDocumentIdList) {
+			idList += "'" + contentDocumentIdList[i] + "',";
+		}
+		idList = idList.slice(0,-1);
+
+		var queryString = "SELECT Id, Title, FileExtension, ContentDocumentId, VersionData FROM ContentVersion WHERE ContentDocumentId IN (" + idList + ")";
+		conn.query(queryString, function(err, result) {
+			if (err) { reject(console.error(err)); }
+
+			console.log("total : " + result.totalSize);
+			console.log("fetched : " + result.records.length);
+			records = result.records;
+			resolve(records);
+		});
+	})
+
+	return promise;
+}
+
+function downloadFile(conn, filePath, contentVersionData) {
+	let promise = new Promise(function(resolve, reject) {
+		const options = {
+			hostname: process.env.DOMAIN_NAME,
+			port: 443,
+			path: contentVersionData,
+			method: 'GET',
+			headers: {
+				'Content-Type': 'application/octet-stream',
+				'Authorization': 'OAuth '+conn.accessToken
+			}
+		}
+
+		https.get(options, (resp) => {
+			let data = '';
+
+			// A chunk of data has been received.
+			resp.on('data', (chunk) => {
+				console.log(chunk);
+				data += chunk;
+
+				fs.appendFile(filePath, chunk, function (err) {
+					if (err) throw err;
+					console.log('chunk updated');
+				});
+
+			});
+
+			// The whole response has been received. Print out the result.
+			resp.on('end', () => {
+				resolve('data downloaded');
+			});
+
+		}).on("error", (err) => {
+			reject(console.log("Error: " + err.message));
+		});
+	});
+
+	return promise;
+}
+
+main();
